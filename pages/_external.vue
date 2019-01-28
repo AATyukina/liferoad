98adc95deb66802a8632011fe0498cc369acc2c1
AATyukina <arina.tyukina@yandex.ru>
2019-01-28 18:11:54 +0300

    second commit

---
+++ pages/_external.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+    asyncData: async function({$axios, data}) {
+      $axios.get('http://185.158.153.91:1380/specialities')
+      .then(function (response) {
+        return {
+          specialities: response.data
+        }
+      })
+      .catch(function (error) {
+        console.log(error); 
+      });
+  }
+   
+}
+</script>
--- pages/specialities/index.vue
+++ pages/specialities/index.vue
@@ -3,18 +3,23 @@
     <div>
       <a href="../">Главная страница</a>
     </div>
-    <div>
-      <h3> Специальности </h3>
+    <div v-if="flag">
+      <div>
+        <h3> Специальности </h3>
+      </div>
+      <div>
+        <ul 
+          v-for="speciality in specialities"
+          :key="speciality"
+          class="list-unstyled">
+          <li >
+            <a :href="speciality.id"> {{ speciality.Название }} </a>
+          </li>
+        </ul> 
+      </div>
     </div>
-    <div>
-      <ul 
-        v-for="speciality in specialities"
-        :key="speciality"
-        class="list-unstyled">
-        <li >
-          <a :href="speciality.id"> {{ speciality.Название }} </a>
-        </li>
-      </ul> 
+    <div v-else> 
+      <h2>Oops! Page not found! But don't worry!</h2>
     </div>
   </div>
 </template>
@@ -22,11 +27,43 @@
 <script>
 export default {
   asyncData: async function({$axios}) {
-    const response = await $axios.get('http://185.158.153.91:1380/specialities')
-    return {
-      specialities: response.data
+    try{
+      const response = await $axios.get('http://185.158.153.91:1380/specialities')
+        return {
+           specialities: response.data,
+           flag:true
+      }
     }
-  }
+    catch (error){
+        return{
+          flag: false
+        }
+    }
+ 
+    // const response = await $axios.get('http://185.158.153.91:1380/specialities')
+    // return {
+    //   specialities: response.data
+    // }
+
+  //     $axios
+  //     .get('http://185.158.153.91:1380/specialities')
+  //     .then(response => {
+  //         console.log(response)
+  //         specialities = response.data
+  //         flag = true 
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //       this.flag = false 
+  //     })
+  //     .finally(() => {
+  //       return {
+  //         specialities, 
+  //         flag
+  //       }
+  //     })
+
+  // }
   // $axios.get('http://185.158.153.91:1380/specialities')
   //     .then(function (response) {
   //       return {
@@ -38,5 +75,6 @@ export default {
   //     });
   // }
   // выводит надпись "Специальности", а дальше не отображает их список
+}
 }
 </script>
